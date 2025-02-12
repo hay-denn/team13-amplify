33aaf867899c8dd7bc8301f5b3139aaae52e7222
jrbrany <jrbrany@clemson.edu>
2025-02-12 15:16:08 -0500

    updating cognito URLs to work with Spring 3 branch

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-
-// Cognito imports
 import { AuthProvider } from "react-oidc-context";
+
+const deployment = "sprint-3";
 
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_jnOjoFtl2",
   client_id: "r7oq53d98cg6a8l4cj6o8l7tm",
   client_secret: "g2os3g88a51u4fltin70j228m18d8l63a3349gmi8ga88j6jf04",
-  redirect_uri: "https://main.d1zgxgaa1s4k42.amplifyapp.com/",
+  redirect_uri: `https://${deployment}.d1zgxgaa1s4k42.amplifyapp.com/`,
   response_type: "code",
   scope: "email openid phone",
   storage: window.localStorage
@@ -18,7 +18,7 @@ const cognitoAuthConfig = {
 
 export const signOutRedirect = () => {
   const clientId = "r7oq53d98cg6a8l4cj6o8l7tm";
-  const logoutUri = "https://main.d1zgxgaa1s4k42.amplifyapp.com/";
+  const logoutUri = `https://${deployment}.d1zgxgaa1s4k42.amplifyapp.com/`;
   const cognitoDomain = "https://us-east-1jnojoftl2.auth.us-east-1.amazoncognito.com";
   
   window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
@@ -31,4 +31,3 @@ createRoot(document.getElementById('root')!).render(
     </AuthProvider>
   </StrictMode>
 );
-  
