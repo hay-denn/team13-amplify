0d8b6b57c69fbf3a7cac10598a26cf4a19e32393
Coderguy101 <mattpollehn@gmail.com>
2025-02-12 13:18:28 -0500

    Added driver dashboard

---
+++ Driver_Dashboard/src/Components/Menu.tsx
@@ -0,0 +1,67 @@
+//Navbar for the Sponsored user
+
+export const MenuInfoSponsoredUser = [
+  {
+    title: "Home",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-house-user",
+  },
+  {
+    title: "About",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-circle-user",
+  },
+  {
+    title: "Catalog",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-store",
+  },
+  {
+    title: "My Points",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-car",
+  },
+  {
+    title: "My Sponsor",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-sitemap",
+  },
+  {
+    title: "My Account",
+    url: "#",
+    cName: "nav-links profile-link",
+    profileImg: "/Images/DefaultProfile.png",
+  },
+];
+
+export const MenuInfoNewUser = [
+  {
+    title: "Home",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-house-user",
+  },
+  {
+    title: "About",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-circle-user",
+  },
+  {
+    title: "My Applications",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-store",
+  },
+  {
+    title: "My Account",
+    url: "#",
+    cName: "nav-links profile-link",
+    profileImg: "/Images/DefaultProfile.png",
+  },
+];
