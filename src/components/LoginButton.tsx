89603c6148bf7d7e7b1234ab982e5ef01e9345c9
hayden :D <hayjroof@gmail.com>
2025-02-17 19:35:31 -0500

    trying to fix sign out again

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,13 +1,26 @@
-import { useAuth } from "react-oidc-context";
+import { AuthContext, useAuth } from "react-oidc-context";
 import { signOutRedirect } from "../main";
 import "./Navbarstyle.css"; //stylesheet
+import { useContext } from "react";
 
 
 export const LoginButton = () => {
     const auth = useAuth();
+    const authContext = useContext(AuthContext);
+
+    const handleLogout = async () => {
+        try {
+        await authContext?.removeUser();
+        signOutRedirect();
+        console.log('User removed from session');
+        } catch (error) {
+        console.error('Error removing user from session', error);
+        }
+    };
+
     if (auth.isAuthenticated) {
         return (          
-            <li className = "nav-links"><i className = "fa-solid" onClick={() => signOutRedirect()}>Sign out</i></li>
+            <li className = "nav-links"><i className = "fa-solid" onClick={() => handleLogout()}>Sign out</i></li>
             );
     } else {
         return (          
