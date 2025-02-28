3349ebffc9cd53e9f24a2cf61cc6638a02057467
jrbrany <jrbrany@clemson.edu>
2025-02-27 22:27:54 -0500

    testing the modal on the driver dashboard

--- src/components/DashBoardHome.tsx
+++ src/components/DashBoardHome.tsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import "./HomeStyles.css";
 import { TopBox } from "./TopBox/TopBox";
 import "bootstrap/dist/css/bootstrap.min.css";
 import CarouselTemplate from "./ImageCycles";
+import { SponsorApplyModal } from "./Modal"; // Importing the modal
+
 interface Props {
   userFName?: string;
   companyName?: string;
 }
 
 export const DashBoardHome = ({ userFName = "User", companyName }: Props) => {
+  const [showModal, setShowModal] = useState(false); // State to control modal visibility
+
   return (
     <>
       <h1 className="welcome">Good Afternoon2, {userFName}!</h1>
@@ -15,7 +20,7 @@ export const DashBoardHome = ({ userFName = "User", companyName }: Props) => {
       {companyName ? (
         <div className="home">
           <div className="box box1">
-            <TopBox></TopBox>
+            <TopBox />
           </div>
           <div className="box box2">
             <b>Current Point Balance</b>
@@ -30,17 +35,22 @@ export const DashBoardHome = ({ userFName = "User", companyName }: Props) => {
             <div className="col-md-5 left-col">
               <h2>Next Steps:</h2>
               <p>
-                Now that you have completed registration as a driver it is time
+                Now that you have completed registration as a driver, it is time
                 for you to start applying to a sponsor of your choice.
               </p>
-              <button className="btn btn-primary mb-3">Apply Now!</button>
+              <button className="btn btn-primary mb-3" onClick={() => setShowModal(true)}>
+                Apply Now!
+              </button>
             </div>
             <div className="col-md-7 right-col">
-              <CarouselTemplate></CarouselTemplate>
+              <CarouselTemplate />
             </div>
           </div>
         </div>
       )}
+
+      {/* Sponsor Apply Modal */}
+      <SponsorApplyModal show={showModal} handleClose={() => setShowModal(false)} userFName={userFName} />
     </>
   );
 };
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,206 +1,207 @@
 import React, { useState, useEffect } from "react";
 import "./Modal.css";
+import { Modal as BootstrapModal, Button, Form } from "react-bootstrap";
 
 async function callAPI(url: string, methodType: string, data: object): Promise<void> {
     try {
-      const response = await fetch(url, {
-        method: methodType, // HTTP method
-        headers: {
-          'Content-Type': 'application/json', // Content type header
-        },
-        body: JSON.stringify(data), // Convert the data to JSON string
-      });
-      if (response.ok) {
-        // If the request was successful
-        const responseData = await response.json();
-        alert('Success: ' + JSON.stringify(responseData)); // Display success alert with response data
-      } else {
-        // Handle error if response status is not OK
-        alert('Error: ' + response.status + ' - ' + response.statusText); // Display error alert with status and message
-      }
+        const response = await fetch(url, {
+            method: methodType,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data),
+        });
+        if (response.ok) {
+            const responseData = await response.json();
+            alert("Success: " + JSON.stringify(responseData));
+        } else {
+            alert("Error: " + response.status + " - " + response.statusText);
+        }
     } catch (error) {
-      // Catch any network or other errors
-      alert('Network Error: ' + error); // Display network error alert
+        alert("Network Error: " + error);
     }
 }
 
-
 interface ModalProps {
-  isOpen: boolean;
-  onClose: () => void;
-  initialData?: { firstName: string; familyName: string; email: string; userType: string; newUser: boolean };
+    isOpen: boolean;
+    onClose: () => void;
+    initialData?: { firstName: string; familyName: string; email: string; userType: string; newUser: boolean };
 }
 
 const userTypes = ["Admin", "Driver", "Sponsor"];
 
 const Modal: React.FC<ModalProps> = ({ isOpen, onClose, initialData }) => {
-  const [firstName, setFirstName] = useState("");
-  const [familyName, setFamilyName] = useState("");
-  const [email, setEmail] = useState("");
-  const [userType, setUserType] = useState(userTypes[1]); // Default to "Driver"
-  const [newUser, setNewUser] = useState(true);
+    const [firstName, setFirstName] = useState("");
+    const [familyName, setFamilyName] = useState("");
+    const [email, setEmail] = useState("");
+    const [userType, setUserType] = useState(userTypes[1]); // Default to "Driver"
+    const [newUser, setNewUser] = useState(true);
 
-  // Populate form fields when initialData changes
-  useEffect(() => {
-    if (initialData) {
-      setFirstName(initialData.firstName);
-      setFamilyName(initialData.familyName);
-      setEmail(initialData.email);
-      setUserType(initialData.userType);
-      setNewUser(initialData.newUser);
-    } else {
-      setFirstName("");
-      setFamilyName("");
-      setEmail("");
-      setUserType(userTypes[1]); // Reset to default
-      setNewUser(true);
-    }
-  }, [initialData, isOpen]);
-
-
-
-  const handleDeleteUser = () => {
-    const DRIVER_URL = "https://o201qmtncd.execute-api.us-east-1.amazonaws.com/dev1";
-    const SPONSOR_URL = "https://v4ihiexduh.execute-api.us-east-1.amazonaws.com/dev1";
-    const ADMIN_URL = "https://adahpqn530.execute-api.us-east-1.amazonaws.com/dev1";
-    if (!newUser) {
-        //create the new user
-        if (userType == "Driver") {
-            const data = {
-            };
-            callAPI(`${DRIVER_URL}/driver?DriverEmail=${encodeURIComponent(email)}`, "DELETE", data);
-        } else if (userType == "Admin") {
-          const data = {
-          };
-          callAPI(`${SPONSOR_URL}/driver?DriverEmail=${encodeURIComponent(email)}`, "DELETE", data);
-        } else if (userType == "Sponsor") {
-          const data = {
-          };
-          callAPI(`${ADMIN_URL}/driver?DriverEmail=${encodeURIComponent(email)}`, "DELETE", data);
+    useEffect(() => {
+        if (initialData) {
+            setFirstName(initialData.firstName);
+            setFamilyName(initialData.familyName);
+            setEmail(initialData.email);
+            setUserType(initialData.userType);
+            setNewUser(initialData.newUser);
         } else {
-          alert("Invalid user type!");
+            setFirstName("");
+            setFamilyName("");
+            setEmail("");
+            setUserType(userTypes[1]); // Reset to default
+            setNewUser(true);
         }
-    }
-    onClose();
-  };
+    }, [initialData, isOpen]);
 
-  const handleSaveUser = () => {
-    const DRIVER_URL = "https://o201qmtncd.execute-api.us-east-1.amazonaws.com/dev1";
-    const SPONSOR_URL = "https://v4ihiexduh.execute-api.us-east-1.amazonaws.com/dev1";
-    const ADMIN_URL = "https://adahpqn530.execute-api.us-east-1.amazonaws.com/dev1";
+    const handleDeleteUser = () => {
+        const DRIVER_URL = "https://o201qmtncd.execute-api.us-east-1.amazonaws.com/dev1";
+        const SPONSOR_URL = "https://v4ihiexduh.execute-api.us-east-1.amazonaws.com/dev1";
+        const ADMIN_URL = "https://adahpqn530.execute-api.us-east-1.amazonaws.com/dev1";
+        if (!newUser) {
+            if (userType === "Driver") {
+                const data = {};
+                callAPI(`${DRIVER_URL}/driver?DriverEmail=${encodeURIComponent(email)}`, "DELETE", data);
+            } else if (userType === "Admin") {
+                const data = {};
+                callAPI(`${SPONSOR_URL}/driver?DriverEmail=${encodeURIComponent(email)}`, "DELETE", data);
+            } else if (userType === "Sponsor") {
+                const data = {};
+                callAPI(`${ADMIN_URL}/driver?DriverEmail=${encodeURIComponent(email)}`, "DELETE", data);
+            } else {
+                alert("Invalid user type!");
+            }
+        }
+        onClose();
+    };
 
-    if (newUser) {
-        //create the new user
-        if (userType == "Driver") {
-            const data = {
-                "DriverEmail": email,
-                "DriverFName": firstName,
-                "DriverLName": familyName,
-            };
-            callAPI(`${DRIVER_URL}/driver`, "POST", data);
-        } else if (userType == "Admin") {
-          const data = {
-               "AdminEmail": email,
-               "AdminFName": firstName,
-               "AdminLName": familyName
-          };
-        callAPI(`${ADMIN_URL}/admin`, "POST", data);
-        } else if (userType == "Sponsor") {
-          const data = {
-            "UserEmail": email,
-            "UserFName": firstName,
-            "UserLName": familyName,
-            "UserOrganization": 1 //this is temporary until sponsor organizations are implemented
-       };
-       callAPI(`${SPONSOR_URL}/sponsor`, "POST", data);
+    const handleSaveUser = () => {
+        const DRIVER_URL = "https://o201qmtncd.execute-api.us-east-1.amazonaws.com/dev1";
+        const SPONSOR_URL = "https://v4ihiexduh.execute-api.us-east-1.amazonaws.com/dev1";
+        const ADMIN_URL = "https://adahpqn530.execute-api.us-east-1.amazonaws.com/dev1";
+
+        if (newUser) {
+            if (userType === "Driver") {
+                const data = {
+                    "DriverEmail": email,
+                    "DriverFName": firstName,
+                    "DriverLName": familyName,
+                };
+                callAPI(`${DRIVER_URL}/driver`, "POST", data);
+            } else if (userType === "Admin") {
+                const data = {
+                    "AdminEmail": email,
+                    "AdminFName": firstName,
+                    "AdminLName": familyName
+                };
+                callAPI(`${ADMIN_URL}/admin`, "POST", data);
+            } else if (userType === "Sponsor") {
+                const data = {
+                    "UserEmail": email,
+                    "UserFName": firstName,
+                    "UserLName": familyName,
+                    "UserOrganization": 1 // Temporary until sponsor organizations are implemented
+                };
+                callAPI(`${SPONSOR_URL}/sponsor`, "POST", data);
+            } else {
+                alert("Invalid user type!");
+            }
         } else {
-          alert("Invalid user type!");
+            if (userType === "Driver") {
+                const data = {
+                    "DriverEmail": email,
+                    "DriverFName": firstName,
+                    "DriverLName": familyName,
+                };
+                callAPI(`${DRIVER_URL}/driver`, "PUT", data);
+            } else if (userType === "Admin") {
+                const data = {
+                    "AdminEmail": email,
+                    "AdminFName": firstName,
+                    "AdminLName": familyName
+                };
+                callAPI(`${ADMIN_URL}/admin`, "PUT", data);
+            } else if (userType === "Sponsor") {
+                const data = {
+                    "UserEmail": email,
+                    "UserFName": firstName,
+                    "UserLName": familyName,
+                    "UserOrganization": 1
+                };
+                callAPI(`${SPONSOR_URL}/sponsor`, "PUT", data);
+            } else {
+                alert("Invalid user type!");
+            }
         }
-    } else {
-        //update an exisitng user
-      if (userType == "Driver") {
-        const data = {
-            "DriverEmail": email,
-            "DriverFName": firstName,
-            "DriverLName": familyName,
+        onClose();
+    };
+
+    if (!isOpen) return null;
+
+    return (
+        <div className="modal-overlay">
+            <div className="modal-content">
+                <button onClick={onClose} className="modal-close-btn">✖</button>
+                <h2>{newUser ? "Create User" : "Edit User"}</h2>
+
+                <input type="text" placeholder="First Name" value={firstName} onChange={(e) => setFirstName(e.target.value)} className="modal-input" />
+                <input type="text" placeholder="Family Name" value={familyName} onChange={(e) => setFamilyName(e.target.value)} className="modal-input" />
+                <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} className="modal-input" />
+
+                <select value={userType} onChange={(e) => setUserType(e.target.value)} className="modal-select">
+                    {userTypes.map((type) => (
+                        <option key={type} value={type}>{type}</option>
+                    ))}
+                </select>
+
+                <button onClick={handleSaveUser} className="modal-button">{newUser ? "Create User" : "Save Changes"}</button>
+            </div>
+        </div>
+    );
+};
+
+// Sponsor Apply Modal added below
+export const SponsorApplyModal = ({ show, handleClose, userFName }: { show: boolean; handleClose: () => void; userFName: string }) => {
+    const [organization, setOrganization] = useState("");
+    const [sponsorUser, setSponsorUser] = useState("");
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        const applicationData = {
+            ApplicationDriver: userFName,
+            ApplicationOrganization: organization,
+            ApplicationSponsorUser: sponsorUser || null,
+            ApplicationStatus: "Pending",
         };
-        callAPI(`${DRIVER_URL}/driver`, "PUT", data);
-      } else if (userType == "Admin") {
-        const data = {
-             "AdminEmail": email,
-             "AdminFName": firstName,
-             "AdminLName": familyName
-        };
-        callAPI(`${ADMIN_URL}/admin`, "PUT", data);
-      } else if (userType == "Sponsor") {
-        const data = {
-          "UserEmail": email,
-          "UserFName": firstName,
-          "UserLName": familyName,
-          "UserOrganization": 1 //this is temporary until sponsor organizations are implemented
-        };
-        callAPI(`${SPONSOR_URL}/sponsor`, "PUT", data);
-      } else {
-        alert("Invalid user type!");
-      }
-    }
-    onClose(); // Close the modal
-  };
 
-  if (!isOpen) return null;
-  //readOnly={!newUser} add this to email to prevent edits when not a new user
+        try {
+            const response = await fetch("https://2ml4i1kz7j.execute-api.us-east-1.amazonaws.com/dev1/driversponsorapplication", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(applicationData),
+            });
 
-  return (
-    <div className="modal-overlay">
-      <div className="modal-content">
-        <button onClick={onClose} className="modal-close-btn">✖</button>
-        <h2>{newUser ? "Create User" : "Edit User"}</h2>
+            if (response.ok) {
+                alert("Application submitted successfully!");
+                handleClose();
+            } else {
+                alert("Failed to submit application.");
+            }
+        } catch (error) {
+            console.error("Error submitting application:", error);
+            alert("An error occurred while submitting the application.");
+        }
+    };
 
-        <input
-          type="text"
-          placeholder="First Name"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-          className="modal-input"
-        />
-        <input
-          type="text"
-          placeholder="Family Name"
-          value={familyName}
-          onChange={(e) => setFamilyName(e.target.value)}
-          className="modal-input"
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="modal-input"
-        />
-
-        <select value={userType} onChange={(e) => setUserType(e.target.value)} className="modal-select">
-          {userTypes.map((type) => (
-            <option key={type} value={type}>
-              {type}
-            </option>
-          ))}
-        </select>
-
-        {/* Show delete button only when editing an existing user */}
-        {!newUser && (
-          <button onClick={handleDeleteUser} className="modal-button delete">
-            Delete User
-          </button>
-        )}
-
-        <button onClick={handleSaveUser} className="modal-button">
-          {newUser ? "Create User" : "Save Changes"}
-        </button>
-      </div>
-    </div>
-  );
+    return (
+        <BootstrapModal show={show} onHide={handleClose} centered backdrop="static">
+            <BootstrapModal.Header closeButton>
+                <BootstrapModal.Title>Apply for a Sponsor</BootstrapModal.Title>
+            </BootstrapModal.Header>
+            <BootstrapModal.Body>
+                <Form onSubmit={handleSubmit}>
+                    <Button variant="primary" type="submit">Submit Application</Button>
+                </Form>
+            </BootstrapModal.Body>
+        </BootstrapModal>
+    );
 };
 
 export default Modal;
-
-
