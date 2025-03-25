61803b98cef177279aa7b9716b5e381dc1c69f6e
Noah Nisbet <nnisbet@clemson.edu>
2025-03-24 22:52:48 -0400

    removing unnecessary import

--- src/pages/APIs/SponsorOrganizationsAPI.tsx
+++ src/pages/APIs/SponsorOrganizationsAPI.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 
 const API_BASE_URL = "https://br9regxcob.execute-api.us-east-1.amazonaws.com/dev1";
 
