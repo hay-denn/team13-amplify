a812f5bbff7050fdcc43c0ca63c702c41caffd80
Noah Nisbet <nnisbet@clemson.edu>
2025-03-26 12:15:47 -0400

    updating home page

--- src/pages/DriverHomeStyles.tsx
+++ src/pages/DriverHomeStyles.tsx
@@ -1,25 +1,35 @@
 import { useAuth } from "react-oidc-context";
+import "./DriverHomeStyles.css";
 
 export default function Home() {
   const auth = useAuth();
 
   return (
-    <div className ="home-page">
-      <h1>Welcome!</h1>
-      {/* Displays the welcome text with a button to sign in */}
-      <p>
-        <a
-          href="#"
-          className="auth-link"
-          onClick={(e) => {
-            e.preventDefault();
-            auth.signinRedirect();
-          }}
-        >
-          Create your account
-        </a> 
-        &nbsp;
-      </p>
+    <div className="home-container">
+      <video
+        className="background-video"
+        src="https://d2elzyqybwozyp.cloudfront.net/cardriving.mp4"
+        autoPlay
+        muted
+        loop
+        playsInline
+      />
+
+    <div className="overlay-content">
+      <h1>Welcome to MoneyMiles</h1>
+      <p className="hero-subtitle">Earn rewards. Drive with purpose.</p>
+      <a
+        href="#"
+        className="auth-bubble"
+        onClick={(e) => {
+          e.preventDefault();
+          auth.signinRedirect();
+        }}
+      >
+        Create your account
+      </a>
+    </div>
+
     </div>
   );
 }
