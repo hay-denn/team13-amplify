ac8705193939a8faa18fa3b6334629c81c677ff1
jrbrany <jrbrany@clemson.edu>
2025-03-27 07:01:17 -0400

    another testing update

--- src/pages/DriverCatalogsTest.tsx
+++ src/pages/DriverCatalogsTest.tsx
@@ -91,10 +91,10 @@ export const DriverCatalogsTest = () => {
 
   const handleTestAddItems = () => {
     const testItems = [
-      { name: "Item A", cost: 10, quantity: 1, org: 4 },
-      { name: "Item B", cost: 15, quantity: 1, org: 7 },
-      { name: "Item C", cost: 20, quantity: 1, org: 4 },
-      { name: "Item D", cost: 5, quantity: 1, org: 7 },
+      { name: "Item A", cost: 10, quantity: 1, org: 3 },
+      { name: "Item B", cost: 15, quantity: 1, org: 3 },
+      { name: "Item C", cost: 20, quantity: 1, org: 3 },
+      { name: "Item D", cost: 5, quantity: 1, org: 3 },
     ];
     testItems.forEach(addToCart);
     alert("Test items added to cart!");
