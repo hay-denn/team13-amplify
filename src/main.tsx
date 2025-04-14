98ebebe1e6bcc49dbad0083f9a5ebd28de0e1e15
jrbrany <jrbrany@clemson.edu>
2025-04-14 06:11:46 -0400

    more env var testing

--- src/main.tsx
+++ src/main.tsx
@@ -5,21 +5,29 @@ import App from './App.tsx';
 import { AuthProvider } from "react-oidc-context";
 import { Amplify } from 'aws-amplify';
 
+console.log("Environment Variables:");
+console.log("VITE_COGNITO_AUTHORITY:", import.meta.env.VITE_COGNITO_AUTHORITY);
+console.log("VITE_CLIENT_ID:", import.meta.env.VITE_CLIENT_ID);
+console.log("VITE_CLIENT_SECRET:", import.meta.env.VITE_CLIENT_SECRET);
+console.log("VITE_REDIRECT_URI:", import.meta.env.VITE_REDIRECT_URI);
+console.log("VITE_COGNITO_DOMAIN:", import.meta.env.VITE_COGNITO_DOMAIN);
+console.log("VITE_IDENTITY_POOL_ID:", import.meta.env.VITE_IDENTITY_POOL_ID);
+
 const cognitoAuthConfig = {
   authority: import.meta.env.VITE_COGNITO_AUTHORITY,
-  client_id: import.meta.env.VITE_COGNITO_CLIENT_ID,
-  client_secret: import.meta.env.VITE_COGNITO_CLIENT_SECRET,
-  redirect_uri: import.meta.env.VITE_COGNITO_REDIRECT_URI,
+  client_id: import.meta.env.VITE_CLIENT_ID,
+  client_secret: import.meta.env.VITE_CLIENT_SECRET,
+  redirect_uri: import.meta.env.VITE_REDIRECT_URI,
   response_type: "code",
   scope: "email openid phone profile aws.cognito.signin.user.admin",
   storage: window.localStorage
 };
 
 export const signOutRedirect = () => {
-  const clientId = import.meta.env.VITE_COGNITO_CLIENT_ID;
-  const logoutUri = import.meta.env.VITE_COGNITO_REDIRECT_URI;
+  const clientId = import.meta.env.VITE_CLIENT_ID;
+  const logoutUri = import.meta.env.VITE_REDIRECT_URI;
   const cognitoDomain = import.meta.env.VITE_COGNITO_DOMAIN;
-
+  
   window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
 };
 
@@ -34,9 +42,9 @@ createRoot(document.getElementById('root')!).render(
 Amplify.configure({
   Auth: {
     Cognito: {
-      userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
-      userPoolClientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
-      identityPoolId: import.meta.env.VITE_COGNITO_IDENTITY_POOL_ID,
+      userPoolId: import.meta.env.VITE_USER_POOL_ID,
+      userPoolClientId: import.meta.env.VITE_CLIENT_ID,
+      identityPoolId: import.meta.env.VITE_IDENTITY_POOL_ID,
       loginWith: {
         email: true, 
       },
@@ -56,4 +64,4 @@ Amplify.configure({
       },
     },
   },
-});
+})
